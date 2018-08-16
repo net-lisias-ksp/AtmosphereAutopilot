a52acee2ad1a4c0cb3be82250bc9aa598fbe8790
Lisias T <me@lisias.net>
2018-08-16 19:52:20 -0300

    Adjusts on the building and deploying phases

--- deploy.sh
+++ deploy.sh
@@ -32,5 +32,7 @@ cp $VERSIONFILE "./GameData/$TARGETDIR"
 cp CHANGE_LOG.md "./GameData/$TARGETDIR"
 cp README.md  "./GameData/$TARGETDIR"
 cp LICENSE "./GameData/$TARGETDIR"
-deploy $PACKAGE
+for dll in AtmosphereAutopilot.UI AtmosphereAutopilot ; do
+    deploy $dll
+done
 
